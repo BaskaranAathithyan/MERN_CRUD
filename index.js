bc7dfa9f2df5b8292f2821ecef46a69d4bed3440
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-01-26 16:11:03 +0530

    start

---
+++ index.js
@@ -0,0 +1,12 @@
+//Third party Module
+
+const express = require("express");
+const app=express();
+
+app.use("/",(req,req) => {
+    resizeBy. 
+})
+
+app.listen(5000,() => {
+    console.log("Server Stated on 5000");
+})
