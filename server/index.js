7c42f7f5d76f65e65325cf093ddf5357ea0c348b
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-02-06 22:21:48 +0530

    insertAndPackagesInstall

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const app=express();
+const cors=require('cors');
 
 const FoodModel= require('./models/Food');
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://aathi27:admin11@crud.jylad.mongodb.net/food?retryWrites=true&w=majority", {
     useNewUrlParser:true,
 })
 
 
-app.get('/',async(req,res) =>{
-    const food = new FoodModel({foodName:"Apple", daysSinceIAte:3});
+app.post('/insert',async(req,res) =>{
+    const foodName = req.body.foodName
+    const days=req.body.days
+    const food = new FoodModel({foodName:foodName, daysSinceIAte:days});
 
     try{
         await food.save();
@@ -20,6 +24,17 @@ app.get('/',async(req,res) =>{
         console.log(err);
     }
 });
+
+
+app.get('/read',async(req,res) =>{
+    FoodModel.find({}, (err,result) => {
+        if(err){
+            res.send(err);
+        }
+        res.send(result);
+    })
+});
+
 
 app.listen(3001, () =>{
     console.log("Server on 3001");
